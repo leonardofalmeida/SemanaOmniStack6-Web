bff550f4e686288995eb24f47b045656a7971bc5
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-04-14 01:46:26 -0300

    Adicionado variaveis de ambiente

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,4 +46,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 //Importando as rotas
 app.use(require("./routes"));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
